161fad59ed73a84311dc368155ea9ac6861d67c8
charan499 <mylavarapusaicharan@gmail.com>
2026-04-02 17:17:17 +0530

    done

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # dombasics
+Desciption:
+Here i have made use of dom objects and changed the greting over input of user,
+onclick of particular box i have changed their background color respectively.
+
+languages used:
+html
+javascript
+
+project structure:
+index.html
+script.js
+
+importing project:
+clone the repository.
+open the index file in the folder.
+give the input.
+open console tab in developers option.
---
+++ script.js
@@ -0,0 +1,26 @@
+function red()
+{
+    document.querySelector(".box1").style.backgroundColor="red";
+
+}
+function blue()
+{
+    document.querySelector(".box2").style.backgroundColor="blue";
+
+}
+function green()
+{
+    document.querySelector(".box3").style.backgroundColor="green";
+
+}
+function yellow()
+{
+    document.querySelector(".box4").style.backgroundColor="yellow";
+
+}
+function greeting()
+{
+ nam=document.querySelector(".inputfield").value;
+ document.querySelector("#name").innerText=nam;
+
+}
